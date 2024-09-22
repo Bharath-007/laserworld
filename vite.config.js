ae3e4abcdd12d2f5d1eb136994fe5f9d1fe20915
Bharath <bharathofficial05@gmail.com>
2024-09-23 00:07:23 +0530

    check if hosted or not

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/", // Add this line to ensure the correct base path for assets
+  base: "/mylaserworld", // Add this line to ensure the correct base path for assets
   build: {
     outDir: "dist", // Ensure that this matches your output folder
   },
